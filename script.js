59e797056425fef0c1b665ce044a40975017334d
logan <loganschef@yahoo.com>
2021-09-06 20:20:26 -0400

    deliverable part 2 first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let happiness=0;
+let energy=0;
+var i=0;
+//var petName;
+//var userInput;
+
+function feed(){
+    energy = energy + 2;
+    i++;
+}
+
+function pet(){
+    happiness = happiness + 1;
+    i++;
+}
+
+function walk(){
+    if(energy == 0){
+        alert("Not enough energy to walk!");
+    } else {
+        happiness = happiness + 2;
+        energy = energy - 1;
+        i++;
+    }
+}
+function petgame(){
+
+    var petName= prompt("What is your pet's name?");
+    
+    do{
+    var userInput= prompt("feed, pet, or walk?");
+
+    if (userInput == "feed"){
+        feed();
+        
+    } else if (userInput == "pet"){
+        pet();
+    } else if (userInput == "walk"){
+        walk();
+    } else {
+        alert("Sorry, not a valid command...");
+    }
+   
+    }  while (i < 6);
+
+    if (i == 6){
+        alert(petName + " has " + happiness + " happiness and " + energy + " energy!");
+    }
+}
